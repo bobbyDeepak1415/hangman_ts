03a741e5403cac4e03be959a6aacb38ea70a4e36
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-07-31 19:18:25 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import words from "./assets/wordList.json";
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    return words[Math.floor(Math.random() * words.length)];
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  return (
+    <div
+      style={{
+        maxWidth: "800px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "2rem",
+        margin: "0 auto",
+      }}
+    >
+      <div style={{ fontSize: "2rem", textAlign: "center", color: "blue" }}>
+        Lose Win
+      </div>
+      <div>
+        <HangmanDrawing/>
+        <HangmanWord/>
+        <Keyboard/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
