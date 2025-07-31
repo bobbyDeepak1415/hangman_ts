cd6715d82a5206ed06cb4d557dbecd0614c43a12
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-07-31 20:21:25 +0530

    added a part of the hangman drawing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { useState } from "react";
-import words from "./assets/wordList.json";
+// import { useState } from "react";
+// import words from "./assets/wordList.json";
+import HangmanDrawing from "./HangmanDrawing";
+import HangmanWord from "./HangmanWord";
+import Keyboard from "./Keyboard";
 
 function App() {
-  const [wordToGuess, setWordToGuess] = useState(() => {
-    return words[Math.floor(Math.random() * words.length)];
-  });
+  // const [wordToGuess, setWordToGuess] = useState(() => {
+  //   return words[Math.floor(Math.random() * words.length)];
+  // });
 
-  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+  // const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
 
   return (
     <div
@@ -23,9 +26,9 @@ function App() {
         Lose Win
       </div>
       <div>
-        {/* <HangmanDrawing/>
+        <HangmanDrawing/>
         <HangmanWord/>
-        <Keyboard/> */}
+        <Keyboard/>
       </div>
     </div>
   );
---
+++ src/HangmanDrawing.tsx
@@ -0,0 +1,29 @@
+export default function HangmanDrawing() {
+  return (
+    <div style={{ position: "relative" }}>
+      <div
+        style={{
+          height: "10px",
+          width: "200px",
+          backgroundColor: "black",
+          marginLeft: "120px",
+        }}
+      ></div>
+      <div
+        style={{
+          height: "500px",
+          width: "10px",
+          backgroundColor: "black",
+          marginLeft: "120px",
+        }}
+      ></div>
+      <div
+        style={{
+          height: "10px",
+          width: "250px",
+          backgroundColor: "black",
+        }}
+      ></div>
+    </div>
+  );
+}
