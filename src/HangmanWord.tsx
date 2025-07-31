8266459fbd0ddd2cc7b5f8f07984b9da59b6d70e
Bobby Deepak <128696259+the-new-guy1415@users.noreply.github.com>
2025-08-01 02:27:15 +0530

    added bothe the hangman drawing and hangman word

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,10 +1,32 @@
-
 export default function HangmanWord() {
-    
-
-    return (
-        <>
-            
-        </>
-    )
+  const word = "test";
+  const guessedLetters = ["t", "g", "h"];
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {word.split("").map((letter, index) => {
+        return (
+          <span style={{ borderBottom: ".1em solid black" }} key={index}>
+            <span
+              style={{
+                visibility: guessedLetters.includes(letter)
+                  ? "visible"
+                  : "hidden",
+              }}
+            >
+              {letter}
+            </span>
+          </span>
+        );
+      })}
+    </div>
+  );
 }
